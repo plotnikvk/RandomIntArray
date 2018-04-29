3171dd29ba9ba8c56f35be87b937dff37eb0e09a
plotnikvk <plotnikov.vova@mail.ru>
2018-04-29 17:52:14 +0300

    First Update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,33 +4,39 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
-Main.randomArray();
-
-	}
-
-    public static void randomArray(){
 		int[]array = new int[20];
+		System.out.println("Массив до изменений:");
 		for (int i = 0; i < array.length; i++) {
-			array[i] = (int)((Math.random()*21)-10);
-			System.out.println(array[i]);
+			array[i] = (int) ((Math.random() * 21) - 10);
+			System.out.print(array[i]+"\t");
 		}
+		System.out.println();
+
 		int biggestNegativeIndex =0;
 		for (int i = 1; i < array.length;i++) {
-			if (array[biggestNegativeIndex] < array[i] && array[i] < 0 ) {
+
+			if ((array[i]<0) &&(array[biggestNegativeIndex] < array[i] || array[biggestNegativeIndex]>=0))  {
 				biggestNegativeIndex = i;
 			}
 		}
 		System.out.println("Max Negative: "+array[biggestNegativeIndex]);
 
-
-
-		int leastPositiveIndex =19;
-		for (int i = 18; i < array.length;i--) {
-			if (array[leastPositiveIndex] > array[i] && array[i] > 0 ) {
+		int leastPositiveIndex = 0;
+		for (int i = 1; i < array.length;i++) {
+			if ((array[i]>0)&&(array[leastPositiveIndex] > array[i] || array[leastPositiveIndex] <= 0)) {
 				leastPositiveIndex = i;
 			}
 		}
 		System.out.println("Min Positive: "+array[leastPositiveIndex]);
-    }
+
+		int temp = array[leastPositiveIndex];
+		array[leastPositiveIndex]=array[biggestNegativeIndex];
+		array[biggestNegativeIndex]=temp;
+
+        System.out.println("Массив после изменений:");
+		for (int i =0; i <array.length; i++){
+			System.out.print(array[i]+"\t");
+		}
+	}
+
 }
