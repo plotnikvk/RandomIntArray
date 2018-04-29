46fa5eae087a2b608587b840c78a02efeba7110d
plotnikvk <plotnikov.vova@mail.ru>
2018-04-29 18:14:27 +0300

    Update 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,37 +3,36 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
-		int[]array = new int[20];
+    public static void main(String[] args) {//Точка входа в приложение
+		int[]array = new int[20];//Создаем массивсостоящий  из 20 целых чисел
 		System.out.println("Массив до изменений:");
-		for (int i = 0; i < array.length; i++) {
-			array[i] = (int) ((Math.random() * 21) - 10);
-			System.out.print(array[i]+"\t");
+		for (int i = 0; i < array.length; i++) {//Циклом for при помощи метода random() класса Math заполняем массив с
+			array[i] = (int) ((Math.random() * 21) - 10);//числами
+			System.out.print(array[i]+"\t");//Выводим массив до изменений на консоль
 		}
 		System.out.println();
 
-		int biggestNegativeIndex =0;
-		for (int i = 1; i < array.length;i++) {
-
+		int biggestNegativeIndex =0;//Заводим переменную для поиска максимального отрицательного числа
+		for (int i = 1; i < array.length;i++) {//находим максимальное отрицательное число
 			if ((array[i]<0) &&(array[biggestNegativeIndex] < array[i] || array[biggestNegativeIndex]>=0))  {
 				biggestNegativeIndex = i;
 			}
 		}
-		System.out.println("Max Negative: "+array[biggestNegativeIndex]);
-
-		int leastPositiveIndex = 0;
-		for (int i = 1; i < array.length;i++) {
+		System.out.println("Max Negative: "+array[biggestNegativeIndex]);//Выводим на консоль максимальное отрицательное
+                                                                         //число
+		int leastPositiveIndex = 0;//Заводим переменную для поиска минимального положительного числа
+		for (int i = 1; i < array.length;i++) {//находим минимальное положительное число
 			if ((array[i]>0)&&(array[leastPositiveIndex] > array[i] || array[leastPositiveIndex] <= 0)) {
 				leastPositiveIndex = i;
 			}
 		}
-		System.out.println("Min Positive: "+array[leastPositiveIndex]);
+		System.out.println("Min Positive: "+array[leastPositiveIndex]);//Выводим на экран минимальное положительное число
 
-		int temp = array[leastPositiveIndex];
+		int temp = array[leastPositiveIndex];//Меняем местами максимальное отрицательное и минимальое положительное числа
 		array[leastPositiveIndex]=array[biggestNegativeIndex];
 		array[biggestNegativeIndex]=temp;
 
-        System.out.println("Массив после изменений:");
+        System.out.println("Массив после изменений:");//Выводим на экран измененный массив
 		for (int i =0; i <array.length; i++){
 			System.out.print(array[i]+"\t");
 		}
